d2dc4a3c2c0c14d2e898e69a8c272b1f120deff0
Yuvasri1101 <yuvasri267@gmail.com>
2024-07-11 19:57:41 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fill the details</h1>
+      <center><form>
+        <table><tr><td>First Name:</td><td><input type='text'></input></td></tr>
+        <tr><td>Last Name:</td><td><input type='text'></input></td></tr>
+        <tr><td>Date of birth:</td><td><input type='text'></input></td></tr></table>
+      </form></center>
+    </div>
+  );
+}
+
+export default App;
